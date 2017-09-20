f6e49dc82c2c06f772de2151cb258a78b13a1e31
Saurabh Barthwal <saurabh@technoscore.net>
2017-09-20 19:09:52 +0530

    some last changes in the code.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.modal';
+
+
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+	selector: 'app-shopping-list',
+	templateUrl: './shopping-list.component.html',
+	styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+	ingredient: Ingredient[] = [
+		new Ingredient('Apples', 20),
+		new Ingredient('Timatoes', 5)
+	];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
